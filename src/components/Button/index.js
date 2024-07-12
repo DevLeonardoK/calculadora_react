2474f397aa2223cda002b8509b15635ac323ddf8
DevLeonardoK <devleonardok@gmail.com>
2024-07-11 21:12:42 -0300

    :white_check_mark:

---
+++ src/components/Button/index.js
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <div className="App">ola</div>;
+};
+
+export default Button;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #000;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 24px;
+  font-family: "Roboto";
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #000;
+    border: 0;
+    outline: none;
+    align: right;
+    font-size: 24px;
+    font-family: "Poppins";
+    text-align: right;
+    padding: 0 15px;
+    color: #fff;
+  }
+`;
